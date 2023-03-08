3b2acb7ca936e3a7e7585489d48a83b0d8b8010e
NEGRETTE88 <127146171+NEGRETTE88@users.noreply.github.com>
2023-03-08 16:54:08 -0300

    CURRICULO

---
+++ boton.js
@@ -0,0 +1,13 @@
+
+
+const btn = document.querySelector(".boton");
+const text = document.querySelector(".leer-mas");
+const wrapper = document.querySelector(".wrapper");
+
+btn.addEventListener("click", e => {
+    text.classList.toggle("leer-mas_open");
+    text.classList.contains("leer-mas_open");
+    
+    btn.innerHTML = "VER MAS" ; btn.innerHTML = "VER MENOS";
+
+});
